0ad7ca7bf3867a1baa029d2f2b5b232b37738388
quiltedBread <quiltedBread@gmail.com>
2022-08-18 23:30:45 -0400

    FIPS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { stateFIPS, countyFIPS } from "./FIPS";
+
 function App() {
-    return <div className="App">app</div>;
+    const [counties, setCounties] = useState({});
+
+    const getCountySubSet = (state: string) => {
+        let subSet: { [key: string]: string } = {};
+        Object.entries(countyFIPS).forEach(([k, v]) => {
+            if (v.slice(0, 2) === state) {
+                subSet[k] = v;
+            }
+        });
+    };
+
+    return (
+        <div className="App">
+            <select onChange={(e) => getCountySubSet(e.target.value)}>
+                {Object.entries(stateFIPS).map(([k, v]) => (
+                    <option key={k} value={v}>
+                        {k}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 }
 
 export default App;

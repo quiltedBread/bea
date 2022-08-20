7080366b4f61eaec6247a96dac162e018823357f
quiltedbread <quiltedbread@gmail.com>
2022-08-20 17:17:32 -0400

    rework fips data

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { stateFIPS, countyFIPS } from "./FIPS";
+import { Dict } from "./types";
 
 function App() {
-    const [counties, setCounties] = useState({});
+    const [state, setState] = useState<keyof typeof stateFIPS | undefined>();
+    const [county, setCounty] = useState<keyof typeof countyFIPS | undefined>();
+    const [countyChoices, setCountyChoices] = useState<string[]>([]);
 
-    const getCountySubSet = (state: string) => {
-        let subSet: { [key: string]: string } = {};
+    const handleStateChange = (e: React.SyntheticEvent) => {
+        const val = (e.target as HTMLSelectElement).value;
+        setState(val);
+        setCountyChoices(getStateCounties(val));
+    };
+
+    const getStateCounties = (state: keyof typeof stateFIPS) => {
+        let counties: string[] = [];
         Object.entries(countyFIPS).forEach(([k, v]) => {
-            if (v.slice(0, 2) === state) {
-                subSet[k] = v;
+            if (k.slice(0, 2) === state) {
+                counties.push(k);
             }
         });
+        return counties;
     };
 
     return (
         <div className="App">
-            <select onChange={(e) => getCountySubSet(e.target.value)}>
-                {Object.entries(stateFIPS).map(([k, v]) => (
-                    <option key={k} value={v}>
-                        {k}
+            <select onChange={handleStateChange} value={state}>
+                {Object.keys(stateFIPS).map((k) => (
+                    <option key={k} value={k}>
+                        {stateFIPS[k]}
                     </option>
                 ))}
             </select>
+            {/* <select
+                onChange={(e) => setCounty(e.target.value)}
+                value={county}
+                placeholder="County"
+            >
+                {counties.map((k) => (
+                    <option key={k} value={k}>
+                        {k}
+                    </option>
+                ))}
+            </select> */}
         </div>
     );
 }
